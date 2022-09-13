ac29dfc6935b40629d99183c77b77b60bfecd0af
Felipe Estevam Luiz <fel13.luiz@gmail.com>
2022-09-13 20:38:33 -0300

    finalizado solucao infra.data

---
+++ src/NetCore6-Mediator-CleanArch.Infra.Data/Identity/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NetCore6_Mediator_CleanArch.Infra.Data.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ src/NetCore6-Mediator-CleanArch.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore6_Mediator_CleanArch.Domain.Entities;
+using NetCore6_Mediator_CleanArch.Domain.Interfaces;
+using NetCore6_Mediator_CleanArch.Infra.Data.Context;
+
+namespace NetCore6_Mediator_CleanArch.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CategoryProductAsync(int? id)
+        {
+            return await _context.Products.FirstOrDefaultAsync(x => x.CategoryId == id.Value) is not null;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryByIdAsync(int? id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task<Category> RemoveAsync(Category category)
+        {
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+    }
+}
